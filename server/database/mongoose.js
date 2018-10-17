46a04ea8dca0d40172cd8012f296c3ae200f5c3a
Mahmoud Soliman <msoliman@zadfresh.com>
2018-10-17 16:32:30 +0200

    [ADD] heroku DB configuration

--- server/database/mongoose.js
+++ server/database/mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 // database values
-const dbUrl = 'mongodb://localhost:27017/';
-const dbName = 'TodoApp';
+const dbUrI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+
 
 // configure Mongoose
 mongoose.Promise = global.Promise;
 
 // connect to databse
-mongoose.connect(dbUrl + dbName, {useNewUrlParser: true});
+mongoose.connect(dbUrI, {useNewUrlParser: true});
 
 
 module.exports = mongoose;
